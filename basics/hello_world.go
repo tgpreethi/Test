2cc644c2d321bc18e3886e668ac5a76543841530
preethitg <tg.preethi108@gmail.com>
2019-07-28 23:09:27 -0700

    Test go basics

---
+++ basics/hello_world.go
@@ -0,0 +1,13 @@
+// https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to Bellevue!")
+	fmt.Println("The time is", time.Now())
+}
